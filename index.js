537d4a3a3b80cd361a0f8471da52bd90a73445bc
Matthew Morales <mtthwmrls9@gmail.com>
2022-05-27 10:52:08 -0600

    Refactoring

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const circles = [];
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
 
-const circle = function (x, y, radius) {
+const Circle = function (x, y, radius) {
     this.radius = radius;
     this.x = x;
     this.y = y;
@@ -18,61 +18,7 @@ const circle = function (x, y, radius) {
     }
 }
 
-const gridLines = function (width, height, cellsX, cellsY)
-{
-    this.height = height;
-    this.width = width;
-    this.cellsX = cellsX;
-    this.cellsY = cellsY;
-
-    this.draw = (ctx) => {
-        cellWidth = width / cellsX;
-        cellHeight = height / cellsY;
-
-        // Vertical Lines
-        for (let y = 0; y < this.height; y += cellHeight)
-        {
-            ctx.beginPath();
-            ctx.moveTo(0, y);
-            ctx.lineTo(width, y);
-            ctx.stroke();
-        }
-
-        // Horizontal Lines
-        for (let x = 0; x < this.width; x += cellWidth)
-        {
-            ctx.beginPath();
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, height);
-            ctx.stroke();
-        }
-    };
-}
-
-const gridValues = function (array2d)
-{
-    this.array2d = array2d;
-
-    this.draw = (ctx, xPos, yPos, offset) => {
-        let xOffset = 0;
-        let yOffset = 0;
-
-        for (let y = 0; y < array2d.length; y++)
-        {
-            for (let x = 0; x < array2d[0].length; x++)
-            {
-                ctx.font = '12px serif';
-                ctx.fillStyle = '#ffffff';
-                ctx.fillText(this.array2d[y][x], xPos + xOffset, yPos + yOffset);
-                xOffset += offset;
-            }
-            xOffset = 0;
-            yOffset += offset;
-        }
-    }
-}
-
-const grid = function (arr, cellSize)
+const Grid = function (arr, cellSize)
 {
     this.arr = arr;
     this.cellSize = cellSize;
@@ -129,18 +75,13 @@ const grid = function (arr, cellSize)
 canvas.width = width;
 canvas.height = height;
 
-circles.push(new circle(250, 250, 100));
-// const debugGrid = new gridLines(height, height, 10, 10);
-// const debugGridValues = new gridValues([["(0,0)", "(0,1)"], ["(1,0)", "(1,1)"]]);
-const myGrid = new grid([["(0,0)", "(0,1)"], ["(1,0)", "(1,1)"]], 100);
-
-console.log(circles);
+circles.push(new Circle(250, 250, 100));
+const myGrid = new Grid([["(0,0)", "(0,1)"], ["(1,0)", "(1,1)"]], 100);
 
 const draw = () => {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // debugGrid.draw(ctx);
-    // debugGridValues.draw(ctx, 25, 25, 100);
+    
     myGrid.drawLines(ctx);
     myGrid.drawValues(ctx, 100)
 
