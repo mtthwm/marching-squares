4c17c71600532b867dd1ac1904017226abe1ba28
Matthew <mtthwmrls9@gmail.com>
2022-05-27 20:38:49 -0600

    Kinda-sorta working?

---
+++ grid.js
@@ -0,0 +1,132 @@
+const GridCell = function (value, color) {
+    this.value = value;
+    this.color = color;
+
+    this.toString = () => {
+        return this.value;
+    };
+};
+
+const Grid = function (width, height, cellSize)
+{
+    this.createFrom2DArray = (arr, cellSize) => {
+        this.arr = arr;
+        this.width = arr[0].length;
+        this.height = arr.length;
+        this.cellSize = cellSize;
+    };
+
+    this.setColor = (x, y, color) => {
+        this.arr[y][x].color = color;
+    };
+
+    this.getColor = (x, y) => {
+        return this.arr[y][x].color;
+    };
+
+    this.setValue = (x, y, value) => {
+        this.arr[y][x].value = value;
+    };
+
+    this.getValue = (x, y) => {
+        return this.arr[y][x].value;
+    };
+
+    this.getCellCorner = (x, y) => {
+        let cornerX = x * this.cellSize;
+        let cornerY = y * this.cellSize;
+
+        return {cornerX, cornerY};
+    };
+
+    this.drawLines = (ctx) => {
+        const width = this.arr[0].length * this.cellSize;
+        const height = this.arr.length * this.cellSize;
+
+        ctx.strokeStyle = "#FFFFFF";
+
+        // Vertical Lines
+        for (let y = 0; y < width; y += cellSize)
+        {
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y);
+            ctx.stroke();
+        }
+
+        // Horizontal Lines
+        for (let x = 0; x < height; x += cellSize)
+        {
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+            ctx.stroke();
+        }
+    };
+
+    this.drawValues = (ctx, offsetX, offsetY) => {
+        this._2DArrayUtil(this.arr, (value, x, y) => {
+            ctx.font = '12px serif';
+            ctx.fillStyle = '#ffffff';
+            ctx.fillText(value.value, x, y, this.cellSize * 0.75);
+        }, offsetX, offsetY);
+    };
+
+    this.drawColors = (ctx) => {
+        this._2DArrayUtil(this.arr, (value, x, y) => {
+            ctx.fillStyle = value.color;
+            ctx.fillRect(x, y, cellSize, cellSize);
+        }, 0, 0);
+    };
+
+    this.drawCoordinates = (ctx) => {
+        this._2DArrayUtil(this.arr, (value, x, y) => {
+            ctx.font = '12px serif';
+            ctx.fillStyle = '#ffffff';
+            ctx.fillText(`(${Math.floor(x / cellSize)},${Math.floor(y / cellSize)})`, x, y, this.cellSize * 0.75);
+        }, cellSize * .4, cellSize * .5);
+    }
+
+    this.forEachCellCoords = (callback) => {
+      this._2DArrayUtil(this.arr, callback, 0, 0);  
+    };
+
+    this.forEachCell = (callback) => {
+        this._2DArrayUtil(this.arr, (element, x, y) => {
+            callback(element, Math.floor(x / cellSize), Math.floor(y / cellSize));
+        }, 0, 0);
+      };
+
+    // Does *something* for every item in a 2D array, calls a callback function, passing in the value, as well as the x and y values on a grid.
+    this._2DArrayUtil = (arr, callback, x = 0, y = 0, ) => {
+        let xOffset = x;
+        let yOffset = y;
+
+        for (let y = 0; y < arr.length; y++)
+        {
+            for (let x = 0; x < arr[0].length; x++)
+            {
+                callback(arr[y][x], xOffset, yOffset);
+                xOffset += this.cellSize;
+            }
+            xOffset = x;
+            yOffset += this.cellSize;
+        }
+    };
+
+    this._generate2DArray = (width, height) => {
+        let arr = [];
+        for (let i = 0; i < height; i++)
+        {
+            let new1DArray = [];
+            for (let j = 0; j < width; j++)
+            {
+                new1DArray.push(new GridCell(null, "#000000"));
+            }
+            arr.push(new1DArray);
+        }
+        return arr;
+    };
+
+    this.createFrom2DArray(this._generate2DArray(width, height), cellSize);
+}
--- index.js
+++ index.js
@@ -18,105 +18,156 @@ const Circle = function (x, y, radius) {
     }
 }
 
-const GridCell = function (value, color) {
-    this.value = value;
-    this.color = color;
-
-    this.toString = () => {
-        return this.value;
-    };
-};
-
-const Grid = function (width, height, cellSize)
-{
-    this.createFrom2DArray = (arr, cellSize) => {
-        this.arr = arr;
-        this.width = arr[0].length;
-        this.height = arr.length;
-        this.cellSize = cellSize;
-    };
-
-    this.drawLines = (ctx) => {
-        const width = this.arr[0].length * this.cellSize;
-        const height = this.arr.length * this.cellSize;
-
-        // Vertical Lines
-        for (let y = 0; y < width; y += cellSize)
-        {
-            ctx.beginPath();
-            ctx.moveTo(0, y);
-            ctx.lineTo(width, y);
-            ctx.stroke();
-        }
-
-        // Horizontal Lines
-        for (let x = 0; x < height; x += cellSize)
-        {
-            ctx.beginPath();
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, height);
-            ctx.stroke();
-        }
-    };
-
-    this.drawValues = (ctx) => {
-        this._2DArrayUtil(this.arr, (value, x, y) => {
-            ctx.font = '12px serif';
-            ctx.fillStyle = '#ffffff';
-            ctx.fillText(value, x, y);
-        }, 25, 25);
-    };
-
-    // Does *something* for every item in a 2D array, calls a callback function, passing in the value, as well as the x and y values on a grid.
-    this._2DArrayUtil = (arr, callback, x = 0, y = 0, ) => {
-        let xOffset = x;
-        let yOffset = y;
-
-        for (let y = 0; y < arr.length; y++)
-        {
-            for (let x = 0; x < arr[0].length; x++)
-            {
-                callback(arr[x][y], xOffset, yOffset);
-                xOffset += this.cellSize;
-            }
-            xOffset = x;
-            yOffset += this.cellSize;
-        }
-    };
-
-    this._generate2DArray = (width, height) => {
-        let arr = [];
-        for (let i = 0; i < height; i++)
-        {
-            let new1DArray = [];
-            for (let j = 0; j < width; j++)
-            {
-                new1DArray.push(new GridCell(null, "#000000"));
-            }
-            arr.push(new1DArray);
-        }
-        return arr;
-    };
-
-    this.createFrom2DArray(this._generate2DArray(width, height), cellSize);
-}
-
 canvas.width = width;
 canvas.height = height;
 
-circles.push(new Circle(250, 250, 100));
+circles.push(new Circle(250, 250, 200));
 const myGrid = new Grid(10, 10, 100);
+
+const circleIntersectFunction = (x, y, circle) => {
+    return ((circle.radius*circle.radius) / (Math.pow(x - circle.x, 2) + Math.pow(y - circle.y, 2)));
+};
 
 const draw = () => {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    myGrid.drawColors(ctx);
     myGrid.drawLines(ctx);
-    myGrid.drawValues(ctx)
+    // myGrid.drawValues(ctx);
+    // myGrid.drawCoordinates(ctx);
+
+    myGrid.forEachCellCoords((element, x, y) => {
+        if (element.value >= 1)
+        {
+            ctx.strokeStyle = "#FF0000";
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 360);
+            ctx.stroke();
+        }
+    });
+
+    myGrid.forEachCell((element, x, y) => {
+        drawTile(ctx, myGrid, x, y)
+    });
 
     circles.forEach((element) => {
         element.draw(ctx);
-    })
+    });
+};
+
+const drawLine = (ctx, x1, y1, x2, y2, color="#FFFFFF") => {
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 }
 
-let interval = setInterval(draw, 1);
+const gridCellToTile = (grid, x, y) => {
+    let tileValue = "";
+    for (let i = y; i <= y + 1; i++)
+    {
+        for (let j = x; j <= x + 1; j++)
+        {
+            if (j >= grid.width || i >= grid.height)
+            {
+                tileValue += "1";
+            }
+            else
+            {
+                tileValue += grid.getValue(j, i) >= 1 ? "1" : "0";
+            }
+        }
+    }
+    return tileValue;
+};
+
+// Returns coordinates for the midpoint of the top edge of a square or a given size, with a corner at the given coordinates
+const topMid = (cornerX, cornerY, cellSize) => {
+    return [cornerX + cellSize / 2, cornerY];
+};
+// Returns coordinates for the midpoint of the left edge of a square or a given size, with a corner at the given coordinates
+const leftMid = (cornerX, cornerY, cellSize) => {
+    return [cornerX, cornerY + cellSize / 2];
+};
+// Returns coordinates for the midpoint of the right edge of a square or a given size, with a corner at the given coordinates
+const rightMid = (cornerX, cornerY, cellSize) => {
+    return [cornerX + cellSize, cornerY + cellSize / 2];
+};
+// Returns coordinates for the midpoint of the bottom edge of a square or a given size, with a corner at the given coordinates
+const bottomMid = (cornerX, cornerY, cellSize) => {
+    return [cornerX + cellSize / 2, cornerY + cellSize];
+};
+
+const tiles = {
+    "0000": (ctx, cornerX, cornerY, cellSize) => {},
+    "1111": (ctx, cornerX, cornerY, cellSize) => {},
+    "0010": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...bottomMid(cornerX, cornerY, cellSize));
+    },
+    "0001": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...bottomMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "0011": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "0100": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...topMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "0110": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...topMid(cornerX, cornerY, cellSize));
+        drawLine(ctx, ...bottomMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "0101": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...topMid(cornerX, cornerY, cellSize), ...bottomMid(cornerX, cornerY, cellSize));
+    },
+    "0111": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...topMid(cornerX, cornerY, cellSize));
+    },
+    "1000": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...topMid(cornerX, cornerY, cellSize));
+    },
+    "1010": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...topMid(cornerX, cornerY, cellSize), ...bottomMid(cornerX, cornerY, cellSize));
+    },
+    "1001": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...topMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...bottomMid(cornerX, cornerY, cellSize));
+    },
+    "1011": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...topMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "1100": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "1110": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...bottomMid(cornerX, cornerY, cellSize), ...rightMid(cornerX, cornerY, cellSize));
+    },
+    "1101": (ctx, cornerX, cornerY, cellSize) => {
+        drawLine(ctx, ...leftMid(cornerX, cornerY, cellSize), ...bottomMid(cornerX, cornerY, cellSize));
+    },
+};
+
+const drawTile = (ctx, grid, x, y) => {
+    const {cornerX, cornerY} = grid.getCellCorner(x, y);
+
+    tiles[gridCellToTile(grid, x, y)](ctx, cornerX, cornerY, grid.cellSize);
+};
+
+const update = () => {
+    circles.forEach((element) => {
+        element.x += 1;
+    });
+
+    myGrid.forEachCellCoords((element, x, y) => {
+        let sum = 0;
+        circles.forEach((circle) => {
+            sum += circleIntersectFunction(x, y, circle);
+        })
+        element.value = sum;
+    });
+};
+
+let drawInterval = setInterval(draw, 40);
+let updateInterval = setInterval(update, 16);
