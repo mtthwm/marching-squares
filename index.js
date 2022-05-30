df56f5b91f58bfec178a492c4024fde8e7f36b26
Matthew <mtthwmrls9@gmail.com>
2022-05-30 14:44:12 -0600

    Trying to get the balls bouncing off walls

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const circles = [];
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
 
-const Circle = function (x, y, radius) {
+const Circle = function (x, y, radius, velocity = new Vec2(0, 0)) {
     this.radius = radius;
     this.x = x;
     this.y = y;
+    this.velocity = velocity;
 
     this.draw = (ctx) => {
         ctx.strokeStyle = circle_color;
@@ -23,13 +24,32 @@ const Circle = function (x, y, radius) {
         ctx.arc(this.x, this.y, this.radius, 0, 360);
         ctx.stroke();
     }
+
+    // this._getXIntersection = (xValue) => {
+    //     const normalDir = this.velocity.normalized();
+    //     const p = new Vec2(this.x, this.y).add(normalDir.multiply(this.radius));
+
+    //     const scalar = xValue / normalDir.x; *
+    //     return new Vec2(this.x, this.y).add(normalDir.multiply(scalar));
+    // };
+
+    this._getIntersectionWithLine = (xInit, yInit, dirX, dirY) => {
+        const p = new Vec2(this.x, this.y).add(this.velocity.normalized().multiply(this.radius));
+        const slope = dirY / dirX;
+
+        const a = ((p.y - yInit) - ((p.x - xInit) * slope)) / (this.velocity.x - this.velocity.y);
+
+        return p.add(this.velocity.multiply(a));
+    };
 }
+
+
 
 canvas.width = width;
 canvas.height = height;
 
-circles.push(new Circle(width / 4, width / 4, width / 5));
-circles.push(new Circle(width / 5, width * 0.45, width / 10));
+circles.push(new Circle(width / 4, width / 4, width / 5, new Vec2(-1, -1.25)));
+circles.push(new Circle(width / 5, width * 0.45, width / 10, new Vec2(1, 0)));
 const myGrid = new Grid(gridSize, gridSize, width / gridSize);
 
 const circleIntersectFunction = (x, y, circle) => {
@@ -42,7 +62,12 @@ const drawLine = (ctx, x1, y1, x2, y2, color="#FFFFFF") => {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
-}
+};
+
+const drawRay = (ctx, position, direction, length, color="#FFFFFF") => {
+    const target = position.add(direction.normalized().multiply(length));
+    return drawLine(ctx, position.x, position.y, target.x, target.y, color);
+};
 
 const gridCellToTile = (grid, x, y) => {
     let tileValue = "";
@@ -66,7 +91,7 @@ const gridCellToTile = (grid, x, y) => {
 const drawBackground = (ctx) => {
     ctx.fillStyle = background_color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+};
 
 const drawCorners = (ctx, grid) => {
     grid.forEachCellCoords((element, x, y) => {
@@ -78,30 +103,41 @@ const drawCorners = (ctx, grid) => {
             ctx.stroke();
         }
     });
-}
+};
 
 const draw = () => {
     drawBackground(ctx);
 
     // myGrid.drawColors(ctx);
-    // myGrid.drawLines(ctx, grid_line_color);
+    myGrid.drawLines(ctx, grid_line_color);
     // myGrid.drawValues(ctx);
     // myGrid.drawCoordinates(ctx);
 
-    // drawCorners(ctx, myGrid);
+    drawCorners(ctx, myGrid);
 
     myGrid.forEachCell((element, x, y) => {
         drawTile(ctx, myGrid, x, y)
     });
 
-    // circles.forEach((element) => {
-    //     element.draw(ctx);
-    // });
+    circles.forEach((element) => {
+        element.draw(ctx);
+        drawRay(ctx, new Vec2(element.x, element.y), element.velocity, 1000);
+        const wallRayPosition = new Vec2(500, 0);
+        const wallRayDirection = new Vec2(0, 1);
+        const collision = circles[1]._getIntersectionWithLine(wallRayPosition.x, wallRayPosition.y, wallRayDirection.x, wallRayDirection.y);
+        drawRay(ctx, wallRayPosition, wallRayDirection, 1000, "#0000FF");
+        ctx.strokeStyle = "#FF00FF";
+        ctx.beginPath();
+        console.log(collision);
+        ctx.arc(collision.x, collision.y, 5, 0, 360);
+        ctx.stroke();
+    });
 };
 
 const update = () => {
     circles.forEach((element) => {
-        element.x += 1;
+        element.x += element.velocity.x;
+        element.y += element.velocity.y;
     });
 
     myGrid.forEachCellCoords((element, x, y) => {
---
+++ vector.js
@@ -0,0 +1,17 @@
+const Vec2 = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.normalized = () => {
+        const mag = this.magnitude();
+        return new Vec2(this.x / mag, this.y / mag);
+    };
+    this.magnitude = () => {
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    };
+    this.multiply = (scalar) => {
+        return new Vec2(this.x * scalar, this.y * scalar);
+    };
+    this.add = (vector) => {
+        return new Vec2(this.x + vector.x, this.y + vector.y);
+    };
+}
